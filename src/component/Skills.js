1bfb671c6addd3d5b335416417b6fc2375136b36
VijaiNatesh <glennvijay@gmail.com>
2022-02-16 13:02:39 +0500

    css correction

--- src/component/Skills.js
+++ src/component/Skills.js
@@ -1,22 +1,30 @@
 import React from 'react'
 import styles from '../../src/myStyles.module.css'
+import mernimage from "../../src/mernimage.jpg"
+
 
 function Skills() {
     return (
 
-        <div className ={styles.skillsDisplay}>
-            <ul>
-                <li className ={styles.list}>HTML</li>
-                <li className ={styles.list}>CSS</li>
-                <li className ={styles.list}>JavaScript</li>
-                <li className ={styles.list}>React</li>
-                <li className ={styles.list}>Node.js</li>
-                <li className ={styles.list}>MongoDB</li>
-                <li className ={styles.list}>Git</li>
-                <li className ={styles.list}>MySQL</li>
-            </ul>
+        <div  >
+            <div className={styles.listDisplay}>
+                <p className={styles.list}>HTML</p>
+                <p className={styles.list}>CSS</p>
+                <p className={styles.list}>JavaScript</p>
+            </div>
+            <div className={styles.listDisplay}>
+                <p className={styles.list}>React</p>
+                <p className={styles.list}>Node.js</p>
+                <p className={styles.list}>MongoDB</p>
+                <p className={styles.list}>Express</p>
+            </div>
+            <div className={styles.listDisplay}>
+                <p className={styles.list}>Git</p>
+                <p className={styles.list}>MySQL</p>
+                <img className={styles.skillimage} src={mernimage} alt="mern" />
+            </div>
+          
         </div>
-
     )
 }
 
