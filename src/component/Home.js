bdb2ce2ba937016cf502d757f1893248c1f4ac56
VijaiNatesh <glennvijay@gmail.com>
2022-03-17 18:14:13 +0500

    corr

--- src/component/Home.js
+++ src/component/Home.js
@@ -6,17 +6,17 @@ import resume from '../../src/Resume.jpg'
 
 function Home() {
     return (
-        <div className = {styles.WebContainer}>
-        <div className = {styles.articles}>
+        <div >
+
             <div className={styles.aboutDisplay}>
                 <p className={styles.homeContent}>Full Stack Developer <br /> based in Namakkal, Tamilnadu</p>
                 <img className={styles.image} src={image} alt="Full Stack Developer" />
             </div>
-            <div className={styles.contactDisplay}>                
+            <div className={styles.contactDisplay}>
                 <a href="https://github.com/VijaiNatesh" target="_blank" title="GitHub" rel="noreferrer"> <img className={styles.contact} src={gitimage} alt="GitHub" /> </a>
                 <a href="https://drive.google.com/file/d/1kcZ9Uc8enjhkScQEE2PZmeUjzPuwL9t1/view?usp=sharing" target="_blank" title="Resume" rel="noreferrer"> <img className={styles.contact} src={resume} alt="resume" /> </a>
             </div>
-            </div>
+
         </div>
 
     )
