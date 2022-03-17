31123ec583c725bd19eb8bdeb5168c16f159c158
VijaiNatesh <glennvijay@gmail.com>
2022-03-17 17:49:51 +0500

    project

--- src/component/Projects.js
+++ src/component/Projects.js
@@ -18,7 +18,7 @@ function Projects() {
                                              User Authentication in Inventory Bill app keeps unauthorized users from accessing sensitive information.</p>
         </div>
         <div className ={styles.projectDisplay}>          
-            <h2 className ={styles.projectTitle}>Student Mentor Assigning <br/>          
+            <h2 className ={styles.projectTitle}>Student Mentor Assigning App <br/>          
             <a className ={styles.projectLink}href = "https://assign-mentor-vj.netlify.app/" target = "_blank" rel="noreferrer">Visit</a><br/>
             <a className ={styles.projectLink}href = "https://github.com/VijaiNatesh/assign-mentor-frontend" target = "_blank" rel="noreferrer">Frontend Code</a><br/>
             <a className ={styles.projectLink}href = "https://github.com/VijaiNatesh/assign-mentor-backend" target = "_blank" rel="noreferrer">Backend Code</a></h2>
@@ -31,6 +31,17 @@ function Projects() {
         </div>
         <div className ={styles.projectDisplay}>          
             <h2 className ={styles.projectTitle}>Reset Password <br/>
+            <a className ={styles.projectLink}href = "https://password-reset-vj.netlify.app/" target = "_blank" rel="noreferrer">Visit</a><br/>
+            <a className ={styles.projectLink}href = "https://github.com/VijaiNatesh/password-reset-frontend" target = "_blank" rel="noreferrer">Frontend Code</a><br/>
+            <a className ={styles.projectLink}href = "https://github.com/VijaiNatesh/password-reset-backend" target = "_blank" rel="noreferrer">Backend Code</a></h2>
+            <p className ={styles.projectDes}> Reset Password was developed with <b>MERN stack</b>.
+                                            It is used in <b>User Authentication</b> of an application.
+                                            If a user of an application forgot his/her password, reset password system will <b>send a link to the registered mail</b> through which 
+                                            user will be able to reset their password.
+                                            </p>
+        </div>
+        <div className ={styles.projectDisplay}>          
+            <h2 className ={styles.projectTitle}>E-Commerce<br/>
             <a className ={styles.projectLink}href = "https://password-reset-vj.netlify.app/" target = "_blank" rel="noreferrer">Visit</a><br/>
             <a className ={styles.projectLink}href = "https://github.com/VijaiNatesh/password-reset-frontend" target = "_blank" rel="noreferrer">Frontend Code</a><br/>
             <a className ={styles.projectLink}href = "https://github.com/VijaiNatesh/password-reset-backend" target = "_blank" rel="noreferrer">Backend Code</a></h2>
