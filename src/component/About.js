b3642cdb82521f3cfed4f5b92349093be6db5c89
VijaiNatesh <glennvijay@gmail.com>
2022-03-23 00:50:44 +0500

    css

--- src/component/About.js
+++ src/component/About.js
@@ -1,28 +1,28 @@
 import React from 'react'
 import styles from '../../src/myStyles.module.css'
-import image from'../../src/NV.jpg'
+import image from '../../src/NV.jpg'
 
 
 function About() {
     return (
-        <div className = {styles.aboutBackground}>
-        <h1>About</h1>
-        <div className={styles.aboutDisplay}>
-            <div>
-                <p className={styles.aboutContentHeader}>Hey, I'm Vijayakumar</p>
-                <p className={styles.aboutContent}>I'm Full Stack Developer based in Namakkal, Tamilnadu.
+        <div className={styles.aboutBackground}>
+            <h1 className={styles.aboutHead}>ABOUT ME</h1>
+            <div className={styles.aboutDisplay}>
+                <img className={styles.myImage} src={image} alt="Vijayakumar" />
+                <div>                    
+                    <p className={styles.aboutContent}>I'm Full Stack Developer based in Namakkal, Tamilnadu.
            Passionate about building web applications that are performant, accessible and have a great user experience.</p>
-                <p className={styles.aboutContent}> I started coding in 2020 after decided to switch my career from teaching to IT sector.
-                I joined Guvi(IIT Madras) to learn MERN Stack.
-                I had build some basic websites and explored difficulties while building the websites.
-                Now, I'm seeking an opportunity in an organization to expose my skills and to contribute the organization's success.
+                    <p className={styles.aboutContent}> I started coding in 2020 after decided to switch my career from teaching to IT sector.
+                    I joined Guvi(IIT Madras) to learn MERN Stack.
+                    I had build some basic websites and explored difficulties while building the websites.
+                    Now, I'm seeking an opportunity in an organization to expose my skills and to contribute the organization's success.
            </p>
-                <p className={styles.aboutContent}>Outside of work I love playing cricket, watching cricket and being outdoors.</p>
+                    <p className={styles.aboutContent}>Outside of work I love playing cricket, watching cricket and being outdoors.</p>
+
+                </div>
 
             </div>
-            <img className={styles.image} src={image} alt="Vijayakumar" />
         </div>
-    </div>
     )
 }
 
--- src/component/Home.js
+++ src/component/Home.js
@@ -9,18 +9,20 @@ import About from './About'
 
 function Home() {
     return (
-        <div className = {styles.container} >
+        <div className ={styles.container} >
 
             <div>
-                <p className={styles.homeContent}>Hi, my name is <br/> <div>VijayaKumar </div> <br/> I'm Full Stack <br/> Developer</p>                
+                <p className={styles.homeContent}>Hi, my name is <br/> <div className ={styles.myName}>Vijayakumar </div> <br/> I'm Full Stack <br/> Developer</p>                
             </div>
+
             <div className={styles.homeIconDisplay}>
                 <a href="https://github.com/VijaiNatesh" target="_blank" title="GitHub" rel="noreferrer"> <img className={styles.contact} src={gitimage} alt="GitHub" /> </a>
                 <a href="https://drive.google.com/file/d/1kcZ9Uc8enjhkScQEE2PZmeUjzPuwL9t1/view?usp=sharing" target="_blank" title="Resume" rel="noreferrer"> <img className={styles.contact} src={resume} alt="resume" /> </a>
+        
             </div>
             <About />
-            <Skills />
-            <Projects/>
+            {/* <Skills />
+            <Projects/> */}
         </div>
 
     )
