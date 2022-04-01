6231b7f24769f76b222ae9382aefb57ba4ad62c1
VijaiNatesh <glennvijay@gmail.com>
2022-04-01 11:44:07 +0500

    css

--- src/component/Home.js
+++ src/component/Home.js
@@ -2,6 +2,7 @@ import React from 'react'
 import styles from '../../src/myStyles.module.css'
 import gitimage from '../../src/Gitimage.png'
 import image from '../../src/NV.jpg'
+import LinkedIn from '../../src/linkedin.png'
 
 
 
@@ -17,7 +18,7 @@ function Home() {
             </div>
             <div className={styles.homeIconDisplay}>
                 <a href="https://github.com/VijaiNatesh" target="_blank" title="Click to view my GitHub account" rel="noreferrer"><img className={styles.contact} src={gitimage} alt="GitHub" /> </a>
-
+                <a href="https://www.linkedin.com/in/vijay-natesh-1a59111aa/" target="_blank" title="Click to view my LinkedIn account" rel="noreferrer"><img className={styles.contact1} src={LinkedIn} alt="LinkedIn" /> </a>
             </div>
         </div>
 
