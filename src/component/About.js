543727a144851df136f8a5ced18fb4a1580d033e
VijaiNatesh <glennvijay@gmail.com>
2022-04-13 13:27:29 +0500

    css

--- src/component/About.js
+++ src/component/About.js
@@ -10,7 +10,7 @@ function About() {
                 <div className = {styles.aboutContent} >
                 <p>A Full Stack Developer
            Passionate about building web applications that are performant, accessible and have a great user experience.
-                 I started coding in 2020 after decided to switch my career from teaching to IT sector.
+                 I started coding in 2021 after decided to switch my career from teaching to IT sector.
                 I joined Guvi(IIT Madras) to learn MERN Stack.
                 I had build some basic websites and explored difficulties while building the websites.
                 Now, I'm seeking an opportunity in an organization to expose my skills and to contribute the organization's success.
